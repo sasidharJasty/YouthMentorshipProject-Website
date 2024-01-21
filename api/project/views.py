949ce02b7a8f4287f58f3d10cffcf7a2dcf7898f
sasidharJasty <88593191+sasidharJasty@users.noreply.github.com>
2024-01-20 20:44:54 -0800

    password-reset update

--- api/project/views.py
+++ api/project/views.py
@@ -34,6 +34,30 @@ from django.contrib.auth.models import Group
 from .serializers import UserGroupCountSerializer
 from rest_framework.viewsets import ViewSet
 
+
+from rest_framework.decorators import api_view, permission_classes
+from rest_framework.permissions import IsAuthenticated
+from rest_framework.response import Response
+from rest_framework import status
+from django.contrib.auth import update_session_auth_hash
+from .serializers import ChangePasswordSerializer
+
+@api_view(['POST'])
+@permission_classes([IsAuthenticated])
+def change_password(request):
+    if request.method == 'POST':
+        serializer = ChangePasswordSerializer(data=request.data)
+        if serializer.is_valid():
+            user = request.user
+            if user.check_password(serializer.data.get('old_password')):
+                user.set_password(serializer.data.get('new_password'))
+                user.save()
+                update_session_auth_hash(request, user)  # To update session after password change
+                return Response({'message': 'Password changed successfully.'}, status=status.HTTP_200_OK)
+            return Response({'error': 'Incorrect old password.'}, status=status.HTTP_400_BAD_REQUEST)
+        return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
+
+
 class UserGroupCountViewSet(ViewSet):
     def list(self, request, group_name=None):
         try:
