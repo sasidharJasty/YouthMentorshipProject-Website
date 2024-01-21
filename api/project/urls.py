0128ee03633ee1c425e5253bcf3f648225857cd2
sasidharJasty <88593191+sasidharJasty@users.noreply.github.com>
2024-01-20 20:45:12 -0800

    Update urls.py

--- api/project/urls.py
+++ api/project/urls.py
@@ -23,5 +23,7 @@ urlpatterns = [
     path('04D2430AAFE10AA4/logout/', views.logout_view, name='logout'),
     path('04D2430AAFE10AA4/signup/', views.SignupView.as_view(), name='signup'),
     path('04D2430AAFE10AA4/group/<str:group_name>/', views.UserGroupCountViewSet.as_view({'get': 'list'}), name='group-count'),
+    path('04D2430AAFE10AA4/password_reset/', include('django_rest_passwordreset.urls', namespace='password_reset')),
+    path('04D2430AAFE10AA4/change_password/', views.change_password, name='change_password'),
     re_path(r'^.*$', TemplateView.as_view(template_name='index.html'), name='index'),
 ]   + static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
