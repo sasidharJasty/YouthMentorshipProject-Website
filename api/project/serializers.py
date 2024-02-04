3a50150f0c22b484e92cc487c15ea66db96fe6ac
COOLCODERVG <varshith.gudeus@gmail.com>
2024-02-04 15:30:54 -0800

    Initial Commit

---
+++ api/project/serializers.py
@@ -0,0 +1,35 @@
+
+from rest_framework import serializers
+from .models import Role, Logs, HourRecord
+from django.contrib.auth import get_user_model
+User = get_user_model()
+
+class UserSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = User
+        fields = '__all__'
+
+class UserGroupCountSerializer(serializers.Serializer):
+    count = serializers.IntegerField()
+
+class RolesSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = Role
+        fields =  '__all__'
+
+class LogsSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = Logs
+        fields = '__all__'
+
+class HoursSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = HourRecord
+        fields = '__all__'
+
+class ChangePasswordSerializer(serializers.Serializer):
+    old_password = serializers.CharField(required=True)
+    new_password = serializers.CharField(required=True)
+
+class ResetPasswordEmailSerializer(serializers.Serializer):
+    email = serializers.EmailField(required=True)
---
+++ api/project/signals.py
@@ -0,0 +1,46 @@
+from django.core.mail import EmailMultiAlternatives
+from django.dispatch import receiver
+from django.template.loader import render_to_string
+from django.urls import reverse
+
+from django_rest_passwordreset.signals import reset_password_token_created
+
+
+@receiver(reset_password_token_created)
+def password_reset_token_created(sender, instance, reset_password_token, *args, **kwargs):
+    """
+    Handles password reset tokens
+    When a token is created, an e-mail needs to be sent to the user
+    :param sender: View Class that sent the signal
+    :param instance: View Instance that sent the signal
+    :param reset_password_token: Token Model Object
+    :param args:
+    :param kwargs:
+    :return:
+    """
+    # send an e-mail to the user
+    context = {
+        'current_user': reset_password_token.user,
+        'username': reset_password_token.user.username,
+        'email': reset_password_token.user.email,
+        'reset_password_url': "{}?token={}".format(
+            instance.request.build_absolute_uri(reverse('password_reset:reset-password-confirm')),
+            reset_password_token.key)
+    }
+
+    # render email text
+    email_plaintext_message = "Hello "+reset_password_token.user.username+", \n\nWe have received a request to reset your password. \nIf this request was not made by you, you can safely ignore this email. \nOtherwise, please click on the link to reset your password:" + "https://{}/reset-password/?token={} \n\nThanks,\nYouth Mentorship Project".format(
+            instance.request.META["HTTP_HOST"],
+            reset_password_token.key) + ""
+
+    msg = EmailMultiAlternatives(
+        # title:
+        "Password Reset for {title}".format(title="Youth Mentorship Project"),
+        # message:
+        email_plaintext_message,
+        # from:
+        "noreply@youthmentorshipproject.org",
+        # to:
+        [reset_password_token.user.email]
+    )
+    msg.send()
